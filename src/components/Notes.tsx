c5e1e07969c271a81b3717c2b80c0ce9222ab7a9
Marina8989 <marina.mill209@gmail.com>
2022-03-13 18:53:14 -0700

    react

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Note} from '../models/note.model';
-
+import {Card, Button} from 'react-bootstrap'
+;
 interface INotesProps {
    note: Note
 }
@@ -8,8 +9,14 @@ interface INotesProps {
 const Notes: React.FC<INotesProps> = ({note}) => {
     return (
       <div className="mt-5">
-        <h2>{note.title}</h2>  
-        <h4>{note.text}</h4>
+        <Card>
+            <Card.Body>
+              <Card.Title>{note.title}</Card.Title>
+              <Card.Text>{note.text}</Card.Text>
+              <Card.Subtitle>{note.date}</Card.Subtitle>
+              <Button className='mb-3' variant='danger'>Delete</Button>
+            </Card.Body>
+        </Card>
       </div>
     )
 }
