c476c8dfbb058df6cec454753f91441d4300e8e4
Marina8989 <marina.mill209@gmail.com>
2022-03-13 17:16:14 -0700

    react ts bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+//import {Note} from './models/note.model';
+import Header from './components/Header';
+
 
 function App() {
-  const name = 'Marina';
+  // const [notes, setNotes] = useState<Note[]>([{
+  //   id: (new Date).toString(),
+  //   title: 'Meetings',
+  //   text: 'Hello, this is just a placeholder text',
+  //   color: '#cccccc',
+  //   date: (new Date).toString()
+  // }])
+
   return (
-    <div className="App">
-      {name}
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Navbar, Container} from 'react-bootstrap';
+
+interface IHeaderProps {
+
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+    return(
+       <Navbar fixed='top' bg='dark' variant='dark'>
+         <Container>
+             <Navbar.Brand>
+                React TypeScript Bootstrap App
+             </Navbar.Brand>
+         </Container>
+       </Navbar>
+    )
+}
+
+export default Header
